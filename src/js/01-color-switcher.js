288ec6c5ae408559c44c10f56c2e282ece685a0a
Evgeniy Pavlichenko <yevheniipavl@gmail.com>
2022-11-18 12:58:12 +0200

    first done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,44 @@
+const btnStartRef = document.querySelector('[data-start]');
+const btnStopRef = document.querySelector('[data-stop]');
+let intervalId;
 
+btnStopRef.disabled = true;
+
+btnStartRef.addEventListener('click', onBtnStartClick);
+btnStopRef.addEventListener('click', onBtnStopClick);
+
+function onBtnStartClick() {
+  switchBtnStatus();
+
+  intervalId = setInterval(changeColor, 1000);
+}
+
+function changeColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function onBtnStopClick() {
+  clearInterval(intervalId);
+
+  switchBtnStatus();
+}
+
+function switchBtnStatus() {
+  btnStartRef.disabled = !btnStartRef.disabled;
+  btnStopRef.disabled = !btnStopRef.disabled;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+// отримати наші кнопки по яких ми будемо клацати
+//є кнопка "старт" і на неї треба натискати. Значить на кнопку старт треба додати прослуховувач події.Які параметри приймає прослуховувач? Тип події та колбек функцію.
+//Тип події - клік
+// пояснення колбек функції
+// робимо неактивною кнопку старт
+//створюємо змінну color і присвоюємо їй результат виклику функції рандомного кольору
+//задаємо колір для боді - document.body.style.backgroundColor = color;
+//при створенні сет інтервала треба записати це все в змінну.
+// раз на секунду - значить використовуємо сет Інтервал при натисканні на кнопку стоп - очищуємо інтервал.
+
+//додаємо на кнопку стоп прослуховувач події. при натисканні на кнопку стоп - передавати цю змінну в clearInterval()
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,53 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  let delay = Number(formEl.elements.delay.value);
+  const step = Number(formEl.elements.step.value);
+  const amount = Number(formEl.elements.amount.value);
+  for (let i = 0; i < amount; i++) {
+    createPromise(i + 1, delay)
+      .then(({ position, delay }) =>
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`,
+          {
+            position: 'center-top',
+          }
+        )
+      )
+      .catch(({ position, delay }) =>
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`,
+          {
+            position: 'center-top',
+          }
+        )
+      );
+    delay += step;
   }
 }
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
