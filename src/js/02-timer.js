5f382d4977bc62f5ea1bb7f165c214c435e04a92
Evgeniy Pavlichenko <yevheniipavl@gmail.com>
2022-11-21 00:26:43 +0200

    finish

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -10,8 +10,10 @@ const daysRef = document.querySelector('[data-days]');
 const hoursRef = document.querySelector('[data-hours]');
 const minsRef = document.querySelector('[data-minutes]');
 const secRef = document.querySelector('[data-seconds]');
-
+// const currentDate = new Date();
 let selectedDate;
+
+console.log(inputRef);
 
 startBtnRef.disabled = true;
 
@@ -21,11 +23,11 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    if (selectedDates[0] <= options.defaultDate) {
+    if (selectedDates[0] < options.defaultDate) {
       Notiflix.Notify.failure('Please choose a date in the future');
     } else {
       Notiflix.Notify.success('Date is chosen');
-
+      console.log(selectedDates[0]);
       startBtnRef.disabled = false;
       selectedDate = selectedDates[0];
     }
@@ -36,13 +38,16 @@ flatpickr(inputRef, options);
 
 startBtnRef.addEventListener('click', onBtnClick);
 function onBtnClick() {
+  // console.log('click');
   const intervalId = setInterval(() => {
     let difference = selectedDate - new Date();
-
+    // const onTime = new Date;
+    // console.log(convertMs(difference));
     if (difference < 0) {
       clearInterval(intervalId);
       return;
     }
+
     renderTimer(convertMs(difference));
   }, 1000);
 }
@@ -52,6 +57,10 @@ function renderTimer(timerObject) {
   hoursRef.textContent = addLeadingZero(timerObject.hours);
   minsRef.textContent = addLeadingZero(timerObject.minutes);
   secRef.textContent = addLeadingZero(timerObject.seconds);
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
 }
 
 function convertMs(ms) {
